5d507c4cb892b4efa410699a19a11b81e8ec4bea
payaltambe999 <tambepayal1234@gmail.com>
2022-02-08 15:19:54 +0530

    Added dependency

---
+++ demo/lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+void main()
+{
+  runApp(MaterialApp(
+    home : Scaffold(
+      appBar : AppBar(
+        title : Text("Hello Word"),
+        centerTitle : true,
+        backgroundColor : Colors.red[600],
+      ),
+      body : Center(
+          child : Text(
+          'Hello Flutter Learining is started',
+          style: TextStyle(
+            fontSize: 20.0,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 2.0,
+            color: Colors.grey[600],
+            fontFamily: 'IndieFlower'
+          ),),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){},
+        child: Text('click'),
+        backgroundColor: Colors.red[600],)
+      ),
+  ),
+
+  );
+}
+
